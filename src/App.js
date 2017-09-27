0a06289e0a7524c6c717cf9f909ffc9c860273d1
delphineli <delphine.li0427@gmail.com>
2017-09-27 15:28:27 +0200

    map with filter

--- src/App.js
+++ src/App.js
@@ -1,65 +1,104 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { compose } from "recompose";
-import {connectHits} from "react-instantsearch/connectors";
-import algoliasearch from 'algoliasearch'
+import { compose, withProps, withStateHandlers } from "recompose";
+import algoliasearch from 'algoliasearch';
+import algoliasearchHelper from 'algoliasearch-helper';
+import styled from 'styled-components';
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
 } from "react-google-maps";
 
+class Popup extends React.Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Votre nom:
+            <input name="name" type="text" />
+          </label>
+          <label>
+            Votre société:
+            <input name="company" type="text" />
+          </label>
+          <label>
+            Votre mobile:
+            <input name="mobile" type="text" />
+          </label>
+          <label>
+            Votre Email:
+            <input name="email" type="text" />
+          </label>
+          <label>
+            Revoir la simulation par :
+            <input name="methode" type="text" />
+          </label>
+          <label>
+            Transmettre à l'admin :
+            <input name="admin" type="checkbox" />
+          </label>
+          <input type="submit" value="Envoyer" />
+        </form>
+          <button onClick={this.props.closePopup}>close me</button>
+      </div>
+    );
+  }
+}
+
+class MarkerWithInfoWindows extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: props.isOpen
+    }
+    this.onToggleOpen = this.onToggleOpen.bind(this);
+  }
+
+  onToggleOpen(){
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
+  render () {
+    return (
+      <Marker
+        position={{ lat:parseFloat(this.props.marker._geoloc.lat), lng: parseFloat(this.props.marker._geoloc.lng) }}
+        onClick={this.onToggleOpen}
+      >
+        {
+          this.state.isOpen && <InfoWindow onCloseClick={this.onToggleOpen}>
+            <p> {this.props.marker.trade} {this.props.marker.cp} {this.props.marker.city} {this.props.marker.cname} </p>
+          </InfoWindow>
+        }
+      </Marker>
+    );
+  }
+}
 
 
 const MapWithAMarker = compose(
+  withStateHandlers(() => ({
+    isOpen: false,
+  })),
   withScriptjs,
   withGoogleMap
 )(props => {
-
-  if(props.markers.length > 0){
-    console.log(props.markers[0]);
-    return (
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-      >
-        { props.markers.map((marker, index) =>
-          <Marker
-            position={{ lat:marker._geoloc.lat, lng: marker._geoloc.lng }}
-          />
-        )}
-      </GoogleMap>
-    );
-  }
-  else{
-    return (
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-      >
-      </GoogleMap>
-    );
-  }
-
+  console.log(props.markers)
+      return (
+        <GoogleMap
+          defaultZoom={10}
+          defaultCenter={{ lat: 48.8566, lng: 2.3522}}
+        >
+        { props.markers.map((marker, index) =>(<MarkerWithInfoWindows marker={marker} key={index} isOpen={props.isOpen} />))}
+        </GoogleMap>
+      );
 });
 
-const ConnectedMap = connectHits(MapWithAMarker);
 
-function Search() {
-  return (
-    <div className="container">
-
-      <ConnectedMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
-  );
-}
 
 class App extends Component {
 
@@ -69,11 +108,21 @@ class App extends Component {
       trade: '',
       pc:'',
       radius:'',
-      markers:[]
+      markers:[],
+      showPopup: false,
+      nbProject: 0,
+      CA: 0
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
   }
 
   handleInputChange(event) {
@@ -83,22 +132,28 @@ class App extends Component {
 
     this.setState({
       [name]: value
+    }, this.doSearch);
+    console.log(this.state.radius)
+  }
+
+  doSearch() {
+    const client = algoliasearch("TIM81UW1UV", "833cbf062e3b7c3979155c0d07e3f058",{protocol: 'https:'});
+    const index = client.initIndex('batimat_dev');
+
+    index.search({
+      query: this.state.pc,
+      facetFilters: [["trade:" + this.state.trade]],
+      aroundRadius: this.state.radius
+    }).then(res => {
+      this.setState({
+        markers: res.hits,
+        nbProject: res.hits.length
+      });
     });
   }
 
   handleSubmit(event) {
      event.preventDefault();
-     alert('A name was submitted: ' + this.state.trade + this.state.pc + this.state.radius);
-    const client = algoliasearch("TIM81UW1UV", "833cbf062e3b7c3979155c0d07e3f058");
-    const index = client.initIndex('batimat_dev');
-    index.search({
-      query: this.state.trade,
-      aroundRadius: this.state.radius
-    }).then(res => {
-      this.setState({
-        markers: res.hits
-      });
-    });
   }
 
   render() {
@@ -111,12 +166,19 @@ class App extends Component {
         <div>
         <form onSubmit={this.handleSubmit}>
           <label>
+            Conseiller:
+            <select name="trade" value={this.state.trade} onChange={this.handleInputChange}>
+              <option value="volvo">François</option>
+              <option value="saab">David</option>
+            </select>
+          </label>
+          <label>
             Votre métier:
             <select name="trade" value={this.state.trade} onChange={this.handleInputChange}>
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="opel">Opel</option>
-              <option value="audi">Audi</option>
+              <option value="patrimoine">patrimoine</option>
+              <option value="fenetres">Fenêtres</option>
+              <option value="veranda">Véranda</option>
+              <option value="Energie solaire">Energie solaire</option>
             </select>
           </label>
           <label>
@@ -125,18 +187,28 @@ class App extends Component {
           </label>
           <label>
             Rayon d'action
-            <input name="radius" type="range" min="100" max="10000" value={this.state.radius} onChange={this.handleInputChange} />
+            <input name="radius" type="range" min="10" max="100" value={this.state.radius} onChange={this.handleInputChange} />
           </label>
 
-          <input type="submit" value="Imprimer" />
+          <input type="submit" onClick={this.togglePopup.bind(this)} value="Imprimer" />
         </form>
-          <MapWithAMarker
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-            markers={this.state.markers}
+        <MapWithAMarker
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          markers={this.state.markers}
+        />
+          <div>
+            <p>{this.state.nbProject} projets trouvés - {this.state.CA} de CA potentiel</p>
+          </div>
+        {this.state.showPopup ?
+          <Popup
+            text='Close Me'
+            closePopup={this.togglePopup}
           />
+          : null
+        }
         </div>
       </div>
     );
